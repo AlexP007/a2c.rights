56a754970d347b3f894f06bd2960715d09c9420c
AlexP <alex.p.panteleev@gmail.com>
2019-10-25 21:35:44 +0300

    adв listeditor

---
+++ lib/editor/cadminlisteditor.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace A2c\Rights\Editor;
+
+use CAdminList;
+
+class CAdminListEditor
+{
+    public function __construct()
+    {
+    }
+}
---
+++ lib/editor/listeditor.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace A2c\Rights\Editor;
+
+use A2c\Rights\User\User;
+
+/**
+ * Class ListEditor
+ * @package A2c\Rights\Editor
+ */
+abstract class ListEditor
+{
+    protected $list;
+    protected $user;
+
+    public function __construct($list, User $user)
+    {
+        $this->list = $list;
+        $this->user = $user;
+    }
+
+    abstract function edit();
+}
